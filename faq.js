ed7a0deaf6b9fab63c313fbffe01bb066b7cb4c3
Leonard Andrei R. Samaniego <leonardsamaniego14@gmail.com>
2025-07-23 17:54:00 +0800

    Accordion style in FAQs

--- faq.js
+++ faq.js
@@ -1,6 +1,12 @@
 document.querySelectorAll('.faq-question').forEach(item => {
     item.addEventListener('click', event => {
+        const currentlyActive = document.querySelector('.faq-item.active');
         const faqItem = item.closest('.faq-item');
+
+        if (currentlyActive && currentlyActive !== faqItem) {
+            currentlyActive.classList.remove('active');
+        }
+
         faqItem.classList.toggle('active');
     });
 });
